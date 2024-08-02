99f12c42d9f4a8d7eea6fe70098597f0ef533716
Maksim-R <rmb89@mail.ru>
2024-08-02 23:05:47 +0500

    - Create new class Login - Create script autorization admin

---
+++ Login.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using SeleniumExtras.WaitHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_example
+{
+    [TestFixture]
+    internal class Login
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            driver.Url = "http://localhost/litecart/admin/";
+            Thread.Sleep(5000);
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            Thread.Sleep(5000);
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            Thread.Sleep(5000);
+            driver.FindElement(By.Name("login")).Click();
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
