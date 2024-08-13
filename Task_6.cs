5fce01393e5f7d6a9bd8d1917d2754cd0216247a
Maksim-R <rmb89@mail.ru>
2024-08-13 23:34:02 +0500

    - Update

--- Task_6.cs
+++ Task_6.cs
@@ -21,79 +21,85 @@ namespace csharp_example
         {
             driver = new ChromeDriver();
             wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-        }
-
-        [Test]
-        public void MenuItems_HaveHeaders()
-        {
             driver.Url = "http://localhost/litecart/admin/";
             driver.FindElement(By.Name("username")).SendKeys("admin");
             driver.FindElement(By.Name("password")).SendKeys("admin");
             driver.FindElement(By.Name("login")).Click();
-            Thread.Sleep(2000);
-
-            // Ищем список меню
-            IWebElement menu = driver.FindElement(By.XPath("//ul[@id='box-apps-menu']"));
-
-            // Ищем все пункты меню
-            IList<IWebElement> menuItems = menu.FindElements(By.XPath("//li"));
-
-            // Обрабатываем каждый пункт меню
-            foreach (IWebElement menuItem in menuItems)
-            {
-                // Ищем ссылку в пункте меню
-                IWebElement link = menuItem.FindElement(By.XPath("//a"));
-
-                // Кликаем по ссылке
-                link.Click();
-
-                // Проверяем наличие заголовка на странице
-                Assert.IsTrue(IsHeaderPresent());
-
-                // Если пункт меню имеет вложенные пункты, обрабатываем их
-                try
-                {
-                    IWebElement subMenu = menuItem.FindElement(By.XPath(".//ul[@class='docs']"));
-                    IList<IWebElement> subMenuItems = subMenu.FindElements(By.XPath("//li"));
-
-                    foreach (IWebElement subMenuItem in subMenuItems)
-                    {
-                        IWebElement subLink = subMenuItem.FindElement(By.XPath(".//a"));
-                        subLink.Click();
-
-                        // Проверяем наличие заголовка на странице
-                        Assert.IsTrue(IsHeaderPresent());
-                    }
-                }
-                catch (NoSuchElementException)
-                {
-                    // Пункт меню не имеет вложенных пунктов
-                }
-
-                // Возвращаемся на страницу с меню
-                driver.Navigate().Back();
-            }
         }
-
-        private bool IsHeaderPresent()
-        {
-            try
-            {
-                IWebElement header = driver.FindElement(By.XPath("//h1"));
-                return true;
-            }
-            catch (NoSuchElementException)
-            {
-                return false;
-            }
-        }
-    
 
         [TearDown]
         public void stop()
         {
             driver.Quit();
             driver = null;
+        }
+        
+        [Test]
+        public void MenuItems_HaveH1Header()
+        {
+            // Находим все пункты меню
+            var menuItems = driver.FindElements(By.XPath("//ul[@id='box-apps-menu']/li"));
+
+            // Обрабатываем каждый пункт меню
+            foreach (var menuItem in menuItems)
+            {
+                // Находим ссылку в пункте меню
+                var link = menuItem.FindElement(By.XPath(".//a"));
+
+                // Кликаем по ссылке
+                link.Click();
+
+                // Ждем пока загрузится страница и появится заголовок h1
+                wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//h1")));
+
+                // Проверяем наличие заголовка h1
+                Assert.IsTrue(IsH1HeaderPresent());
+            }
+        }
+
+        [Test]
+        public void SubMenuItems_HaveH1Header()
+        {
+            // Находим все пункты меню
+            var menuItems = driver.FindElements(By.XPath("//ul[@id='box-apps-menu']/li"));
+
+            // Обрабатываем каждый пункт меню
+            foreach (var menuItem in menuItems)
+            {
+                // Если пункт меню имеет вложенные пункты, обрабатываем их
+                try
+                {
+                    var subMenuItems = menuItem.FindElements(By.XPath(".//ul[@class='docs']/li"));
+                    foreach (var subMenuItem in subMenuItems)
+                    {
+                        var subLink = subMenuItem.FindElement(By.XPath(".//a"));
+                        subLink.Click();
+
+                        // Ждем пока загрузится страница и появится заголовок h1
+                        wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//h1")));
+
+                        // Проверяем наличие заголовка h1
+                        Assert.IsTrue(IsH1HeaderPresent());
+                    }
+                }
+                catch (NoSuchElementException)
+                {
+                    // Пункт меню не имеет вложенных пунктов
+                }
+            }
+        }
+
+        private bool IsH1HeaderPresent()
+        {
+            try
+            {
+                var h1 = driver.FindElement(By.XPath("//h1"));
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
         }
     }
 }
